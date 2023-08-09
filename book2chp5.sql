1b5336cabf0ac8e05ef7b9d40de02f89e164533f
Grayson Smith <graysonsmith91@gmail.com>
2023-08-09 14:08:59 -0500

    example 4

--- book2chp5.sql
+++ book2chp5.sql
@@ -13,3 +13,34 @@ INNER JOIN customers c
    ON c.customer_id = s1.customer_id
 GROUP BY c.first_name, c.last_name, s1.invoice_number, s1.pickup_date
 ORDER BY s1.pickup_date;
+
+
+
+-- Get employees and customers who have interacted through a sale.
+-- Include employees who may not have made a sale yet.
+-- Include customers who may not have completed a purchase.
+
+SELECT
+    e.first_name AS employee_first_name,
+    e.last_name AS employee_last_name,
+    c.first_name AS customer_first_name,
+    c.last_name AS customer_last_name
+FROM employees e
+FULL OUTER JOIN sales s ON e.employee_id = s.employee_id
+FULL OUTER JOIN customers c ON s.customer_id = c.customer_id;
+
+
+
+-- Get a list of all dealerships and which roles each of the employees hold.
+--SELECT
+--    d.business_name,  
+--    e.first_name, 
+--    et.employee_type_name 
+--FROM dealerships d
+--LEFT JOIN dealershipemployees de ON d.dealership_id = de.dealership_id
+--INNER JOIN employees e ON de.employee_id = e.employee_id
+--RIGHT JOIN employeetypes et ON e.employee_type_id = et.employee_type_id
+--ORDER BY d.business_name;
+
+
+
