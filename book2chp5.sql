f28c266d764ac3dfb1b3f4de2eb415409acd249b
Grayson Smith <graysonsmith91@gmail.com>
2023-08-08 19:34:08 -0500

    book2chp5

---
+++ book2chp5.sql
@@ -0,0 +1,12 @@
+-- Use a self join to list all sales that will be picked up on the same day,
+-- including the full name of customer picking up the vehicle. .
+SELECT
+    CONCAT  (c.first_name, ' ', c.last_name) AS last_name,
+    s1.invoice_number,
+    s1.pickup_date
+FROM sales s1
+INNER JOIN sales s2
+    ON s1.sale_id <> s2.sale_id 
+    AND s1.pickup_date = s2.pickup_date
+INNER JOIN customers c
+   ON c.customer_id = s1.customer_id;
