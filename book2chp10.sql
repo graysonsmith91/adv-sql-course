9c96d990ed4c3514b5f3ea134c91652a9fc8b056
Grayson Smith <graysonsmith91@gmail.com>
2023-08-16 15:19:36 -0500

    practice Q #2

--- book2chp10.sql
+++ book2chp10.sql
@@ -8,3 +8,16 @@ LEFT JOIN vehicletypes vt ON vt.vehicle_type_id = v.vehicle_type_id
 WHERE v.is_sold = FALSE
 ORDER BY num_model
 LIMIT 1;
+
+
+
+-- Which model of vehicle has the highest current inventory? This will help dealerships know which models are, perhaps, not selling.
+
+SELECT DISTINCT 
+	vt.model,
+	COUNT(vt.model) OVER(PARTITION BY vt.model) num_model
+FROM vehicles v 
+LEFT JOIN vehicletypes vt ON vt.vehicle_type_id = v.vehicle_type_id 
+WHERE v.is_sold = FALSE
+ORDER BY num_model DESC
+LIMIT 1;
