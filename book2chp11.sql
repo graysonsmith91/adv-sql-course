acef9016afde38526ab1907670b7a118f34b8640
Grayson Smith <graysonsmith91@gmail.com>
2023-08-17 16:39:52 -0500

    question #2

--- book2chp11.sql
+++ book2chp11.sql
@@ -6,3 +6,17 @@ SELECT DISTINCT
 FROM employees e 
 LEFT JOIN employeetypes et ON et.employee_type_id  = e.employee_type_id 
 ORDER BY et.employee_type_name;
+
+
+
+-- How many finance managers work at each dealership?
+
+SELECT DISTINCT 
+	d.business_name, 
+	COUNT(e.employee_type_id) OVER(PARTITION BY d.dealership_id) fm_per_dealership
+FROM employees e 
+LEFT JOIN employeetypes et ON et.employee_type_id = e.employee_type_id 
+LEFT JOIN dealershipemployees de ON de.employee_id = e.employee_id 
+LEFT JOIN dealerships d ON d.dealership_id = de.dealership_id 
+WHERE et.employee_type_name = 'Finance Manager'
+ORDER BY d.business_name;
