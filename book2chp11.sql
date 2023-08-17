cdee61a79a30eb86daa5438e57d156959e341037
Grayson Smith <graysonsmith91@gmail.com>
2023-08-17 16:47:55 -0500

    question #3

--- book2chp11.sql
+++ book2chp11.sql
@@ -20,3 +20,19 @@ LEFT JOIN dealershipemployees de ON de.employee_id = e.employee_id
 LEFT JOIN dealerships d ON d.dealership_id = de.dealership_id 
 WHERE et.employee_type_name = 'Finance Manager'
 ORDER BY d.business_name;
+
+
+
+-- Get the names of the top 3 employees who work shifts at the most dealerships?
+
+SELECT DISTINCT 
+	e.first_name || ' ' || e.last_name full_name,
+	COUNT(de.employee_id) OVER(PARTITION BY	de.employee_id) num_jobs
+FROM dealershipemployees de
+LEFT JOIN dealerships d ON d.dealership_id = de.dealership_id 
+LEFT JOIN employees e ON e.employee_id = de.employee_id 
+ORDER BY num_jobs DESC
+LIMIT 3;
+
+
+
