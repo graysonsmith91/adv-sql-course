31a4357bef62567b5497df5d384dc3d43df24dbd
Grayson Smith <graysonsmith91@gmail.com>
2023-08-17 20:16:13 -0500

    question #2

--- book2chp12.sql
+++ book2chp12.sql
@@ -1,11 +1,28 @@
 -- What are the top 5 US states with the most customers who have purchased a vehicle from a dealership participating in the Carnival platform?
 
 SELECT 
-	d.state,
-	COUNT(s.employee_id) AS num_customers
+	c.state,
+	COUNT(s.customer_id) AS num_customers
 FROM sales s 
 LEFT JOIN dealerships d ON d.dealership_id = s.dealership_id 
+LEFT JOIN customers c ON c.customer_id = s.customer_id 
 WHERE s.sales_type_id = 1
-GROUP BY d.state 
+GROUP BY c.state 
 ORDER BY num_customers DESC 
 LIMIT 5;
+
+
+
+-- What are the top 5 US zipcodes with the most customers who have purchased a vehicle from a dealership participating in the Carnival platform?
+
+SELECT 
+	c.zipcode,
+	COUNT(s.customer_id) AS num_customers
+FROM sales s 
+LEFT JOIN dealerships d ON d.dealership_id = s.dealership_id 
+LEFT JOIN customers c ON c.customer_id = s.customer_id 
+WHERE s.sales_type_id = 1
+GROUP BY c.zipcode
+ORDER BY num_customers DESC 
+LIMIT 5;
+
