de88aa1428055ff5cac3c667a57863b3b3a04ba9
Grayson Smith <graysonsmith91@gmail.com>
2023-08-17 20:26:16 -0500

    question #3

--- book2chp12.sql
+++ book2chp12.sql
@@ -26,3 +26,19 @@ GROUP BY c.zipcode
 ORDER BY num_customers DESC 
 LIMIT 5;
 
+
+
+-- What are the top 5 dealerships with the most customers?
+
+SELECT
+	d.business_name,
+	COUNT(s.customer_id) AS num_customers
+FROM sales s 
+LEFT JOIN dealerships d ON d.dealership_id = s.dealership_id 
+GROUP BY d.business_name 
+ORDER BY num_customers DESC
+LIMIT 5;
+
+
+
+
