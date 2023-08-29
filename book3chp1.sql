19a7ac001ebdc2e815ad83a27f34df30b4d67cd0
Grayson Smith <graysonsmith91@gmail.com>
2023-08-29 11:26:01 -0500

    chp 1 example and setup

---
+++ book3chp1.sql
@@ -0,0 +1,11 @@
+-- UPDATE statement to change a customer
+UPDATE  public.customers
+SET email = 'juliasmith@gmail.com'
+WHERE customer_id = 67;
+
+
+
+-- Kristopher Blumfield an employee of Carnival has asked to be transferred to a different dealership location. 
+-- She is currently at dealership 9. She would like to work at dealership 20. Update her record to reflect her transfer.
+
+
