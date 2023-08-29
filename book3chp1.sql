02f6cb6822518d8167226439718218ba1285eb9c
Grayson Smith <graysonsmith91@gmail.com>
2023-08-29 11:29:32 -0500

    question #1

--- book3chp1.sql
+++ book3chp1.sql
@@ -8,4 +8,11 @@ WHERE customer_id = 67;
 -- Kristopher Blumfield an employee of Carnival has asked to be transferred to a different dealership location. 
 -- She is currently at dealership 9. She would like to work at dealership 20. Update her record to reflect her transfer.
 
+SELECT * FROM employees e 
+LEFT JOIN dealershipemployees de ON de.dealership_employee_id = e.employee_id 
+WHERE de.employee_id = 9
+
+UPDATE dealershipemployees
+SET dealership_id = 20
+WHERE employee_id = 9
 
