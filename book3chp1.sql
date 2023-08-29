f1ee5acfb896ea49cb6a97392f9f9e070ec2b23d
Grayson Smith <graysonsmith91@gmail.com>
2023-08-29 11:44:00 -0500

    question #2

--- book3chp1.sql
+++ book3chp1.sql
@@ -16,3 +16,19 @@ UPDATE dealershipemployees
 SET dealership_id = 20
 WHERE employee_id = 9
 
+
+
+-- A Sales associate needs to update a sales record because her customer want to pay with a Mastercard instead of JCB. 
+-- Update Customer, Ernestus Abeau Sales record which has an invoice number of 9086714242.
+
+SELECT * FROM sales s 
+LEFT JOIN customers c ON c.customer_id = s.customer_id 
+WHERE s.invoice_number = '9086714242'
+
+UPDATE sales
+SET payment_method = 'mastercard'
+WHERE invoice_number = '9086714242'
+
+
+
+
