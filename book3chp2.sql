44ed0b2c048aeb428ac05b8d8d9999eeb0a6fd09
Grayson Smith <graysonsmith91@gmail.com>
2023-08-29 19:02:44 -0500

    practice question #1

--- book3chp2.sql
+++ book3chp2.sql
@@ -1,2 +1,12 @@
 -- DELETE statement to delete one specific record:
 DELETE FROM sales WHERE sales.sale_id = 100;
+
+
+
+-- A sales employee at carnival creates a new sales record for a sale they are trying to close. The customer, last minute 
+-- decided not to purchase the vehicle. Help delete the Sales record with an invoice number of '2436217483'.
+
+SELECT * FROM sales s 
+WHERE s.invoice_number = '2436217483'
+
+DELETE FROM sales WHERE sales.invoice_number = '2436217483'
