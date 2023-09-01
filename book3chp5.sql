55cdfa4314bdb560419b66104cc44144ff06b5b3
Grayson Smith <graysonsmith91@gmail.com>
2023-09-01 14:07:17 -0500

    example complete, working correctly

--- book3chp5.sql
+++ book3chp5.sql
@@ -13,3 +13,16 @@ BEGIN
   RETURN NULL;
 END;
 $$
+
+CREATE TRIGGER new_sale_made
+  AFTER INSERT
+  ON sales
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_pickup_date();
+  
+ SELECT * FROM sales s 
+ 
+INSERT INTO Sales (sales_type_id, vehicle_id, employee_id, customer_id, dealership_id, price, deposit, purchase_date, pickup_date, invoice_number, payment_method, sale_returned)
+VALUES
+  (1, 1, 1, 1, 1, 15000.00, 2000.00, '2023-09-01', '2023-09-10', 'INV12345', 'Credit Card', false);
+
