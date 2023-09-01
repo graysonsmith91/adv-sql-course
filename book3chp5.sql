a49ab9ba26e6ba74e516711c49cffb5d0373f54a
Grayson Smith <graysonsmith91@gmail.com>
2023-09-01 13:55:52 -0500

    example setup

---
+++ book3chp5.sql
@@ -0,0 +1,15 @@
+-- Our first step is to define our trigger function. The keyword NEW allows us to access the data in the newly inserted row.
+
+CREATE FUNCTION set_pickup_date() 
+  RETURNS TRIGGER 
+  LANGUAGE PlPGSQL
+AS $$
+BEGIN
+  -- trigger function logic
+  UPDATE sales
+  SET pickup_date = NEW.purchase_date + integer '7'
+  WHERE sales.sale_id = NEW.sale_id;
+  
+  RETURN NULL;
+END;
+$$
