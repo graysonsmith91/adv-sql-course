bb696b8b3d766aefa6df4bdef4c1cb83cc27849a
Grayson Smith <graysonsmith91@gmail.com>
2023-08-31 19:41:44 -0500

    question #2

--- book3chp4.sql
+++ book3chp4.sql
@@ -24,17 +24,22 @@ SELECT * FROM vehicles v
 -- Carnival would also like to handle the case for when a car gets returned by a customer. When this occurs they must add 
 -- the car back to the inventory and mark the original sales record as sale_returned = TRUE
 
-CREATE OR REPLACE PROCEDURE return_vehicle(IN vehicle_id int, OUT status varchar)
+CREATE OR REPLACE PROCEDURE return_vehicle(IN vehicleId int)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    UPDATE sales s
-    SET s.sale_returned = true
-    WHERE s.vehicle_id = vehicle_id;
-   
-    status := 'Vehicle returned';
+    UPDATE sales
+    SET sale_returned = true
+    WHERE vehicle_id = vehicleId;
   
+	UPDATE vehicles
+	SET is_sold = false
+	WHERE vehicle_id = vehicleId;
+   
     COMMIT;
 END
 $$;
+
+CALL return_vehicle(1);
+
 
