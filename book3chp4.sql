e8e5a688ac0ad665bb1f9fe473f50f28369494ae
Grayson Smith <graysonsmith91@gmail.com>
2023-08-31 19:08:02 -0500

    question #1 complete

--- book3chp4.sql
+++ book3chp4.sql
@@ -1,20 +1,40 @@
 -- Carnival would like to create a stored procedure that handles the case of updating their vehicle inventory when a sale occurs. 
 -- They plan to do this by flagging the vehicle as is_sold which is a field on the Vehicles table
 
-CREATE PROCEDURE update_vehicle(IN vehicle_id int, INOUT b varchar)
+CREATE OR REPLACE PROCEDURE update_vehicle(IN vehicleId int)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-
-UPDATE vehicles v
-    SET v.is_sold = true
-    WHERE v.vehicle_id = vehicle_id;
+    UPDATE vehicles
+    SET is_sold = true
+    WHERE vehicle_id = vehicleId;
    
-   status := 'Vehicle sold';
-  
-  COMMIT;
-
+    COMMIT;
 END
 $$;
 
+CALL update_vehicle(1); 
+
+DROP PROCEDURE update_vehicle(integer)
+
+SELECT * FROM vehicles v 
+
+
+
+-- Carnival would also like to handle the case for when a car gets returned by a customer. When this occurs they must add 
+-- the car back to the inventory and mark the original sales record as sale_returned = TRUE
+
+CREATE OR REPLACE PROCEDURE return_vehicle(IN vehicle_id int, OUT status varchar)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE sales s
+    SET s.sale_returned = true
+    WHERE s.vehicle_id = vehicle_id;
+   
+    status := 'Vehicle returned';
+  
+    COMMIT;
+END
+$$;
 
