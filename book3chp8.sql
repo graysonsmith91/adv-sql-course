cd01466d3e10aff2d1b72c36705bab9e3e8cdba1
Grayson Smith <graysonsmith91@gmail.com>
2023-09-07 11:03:42 -0500

    transaction question #3

--- book3chp8.sql
+++ book3chp8.sql
@@ -54,3 +54,62 @@ END;
 
 $$ language plpgsql;
 
+
+
+-- The vehicle with VIN KNDPB3A20D7558809 is about to be returned. Carnival has a pretty cool program where it offers the returned vehicle 
+-- to the most recently hired employee at 70% of the cost it previously sold for. The most recent employee accepts this offer and will purchase 
+-- the vehicle once it is returned. The employee and dealership who sold the car originally will be on the new sales transaction.
+
+DO $$ 
+DECLARE 
+  CurrentTS date;
+  sale_id_to_update integer;
+ 
+BEGIN
+  -- Find the sale_id to update
+  SELECT s.sale_id
+  INTO sale_id_to_update
+  FROM sales s
+  JOIN vehicles v ON v.vehicle_id = s.vehicle_id 
+  WHERE v.vin = 'KNDPB3A20D7558809'
+  ORDER BY s.purchase_date DESC
+  LIMIT 1;
+
+  -- Update the sale_returned column for the found sale
+  UPDATE sales
+  SET sale_returned = TRUE
+  WHERE sale_id = sale_id_to_update;
+
+  -- Insert a new sale record with modified data
+  INSERT INTO sales(
+    sales_type_id,
+    vehicle_id,
+    employee_id,
+    customer_id,
+    dealership_id,
+    price,
+    deposit,
+    purchase_date,
+    pickup_date,
+    invoice_number,
+    payment_method
+  )
+  VALUES (
+    1,
+    651,
+    110,
+    550,
+    44,
+    91539.56 * 0.7, -- 70% of the previous sale price
+    1900,
+    CurrentTS,
+    CurrentTS + interval '7 days',
+    1273592748,
+    'jcb'
+  );
+   
+END;
+$$ LANGUAGE plpgsql;
+
+
+
