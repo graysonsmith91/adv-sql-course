79e6b208454ae57a465530a8d27bd8d2795410e1
Grayson Smith <graysonsmith91@gmail.com>
2023-09-07 10:28:48 -0500

    added transaction to update unsold mazdas

--- book3chp8.sql
+++ book3chp8.sql
@@ -27,3 +27,30 @@ BEGIN
 END;
 
 $$ language plpgsql;
+
+
+
+-- For the CX-5s and CX-9s in the inventory that have not been sold, change the year of the car to 2021 since we will be updating our stock of Mazdas. 
+-- For all other unsold Mazdas, update the year to 2020. The newer Mazdas all have red and black interiors.
+
+-- cx-5 and cx-9 ids: 6, 9
+
+do $$ 
+DECLARE 
+
+BEGIN
+  UPDATE vehicles 
+  SET year_of_car = 2021, interior_color = 'Red & Black'
+  WHERE (vehicle_type_id = 3 OR vehicle_type_id = 6 OR vehicle_type_id = 9)
+  AND is_sold = FALSE;
+   
+
+--  UPDATE vehicles 
+--  SET year_of_car = 2020
+--  WHERE (vehicle_type_id = 3 OR vehicle_type_id = 6 OR vehicle_type_id = 9)
+--  AND is_sold = FALSE;
+   
+END;
+
+$$ language plpgsql;
+
